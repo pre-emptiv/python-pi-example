0f85ba200203cef8cdcf3e45be2bc5137f6ff7e8
pre-emptiv <blackthawttech@gmail.com>
2017-10-11 00:30:36 -0400

    Newest comment from dual changes

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@
 def main(msg):
 	 // Adding a different comment
 	// Prints a message
+    // Adding a third message
     print(msg)
 
 main("Hello People")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-pi-example
 THis is an example project to demonstrate the use of git on Ubuntu
+
+
+Make a change
